c6ddad6cae31ea64b42e85e70ecb230ba61aa035
Daniel Schulz <daniel.schulz@ocono.de>
2017-12-01 23:18:26 +0100

    Add favicon

--- service.go
+++ service.go
@@ -4,15 +4,25 @@ import (
 	"net/http"
 )
 
-type MartialartsTrackerService struct {
+type FavIconService struct {
 }
 
-func (s MartialartsTrackerService) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	for i := 0; i < 10; i++ {
-		rw.Write([]byte("JKD"))
-	}
+func (s FavIconService) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Add("Content-Type", "image/x-icon")
+	http.ServeFile(rw, r, "/go/src/github.com/DanShu93/martialarts-tracker/favicon.ico")
 }
+
+type MainService struct {
+}
+
+func (s MainService) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Add("Content-Type", "text/html")
+	rw.Write([]byte("<link rel=\"shortcut icon\" href=\"http://localhost:8888/favicon.ico\" type=\"image/x-icon\">"))
+}
+
 
 func main() {
-	http.ListenAndServe(":80", MartialartsTrackerService{})
+	http.Handle("/favicon.ico", FavIconService{})
+	http.Handle("/index.html", MainService{})
+	http.ListenAndServe(":80", nil)
 }
