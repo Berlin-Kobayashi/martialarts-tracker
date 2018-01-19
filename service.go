7d48bbe6d73c67d7df5a48eddeb1bccedaef184e
Daniel Schulz <daniel.schulz@ocono.de>
2018-01-19 23:34:49 +0100

    Add TrainingUnitService

--- service.go
+++ service.go
@@ -2,7 +2,34 @@ package main
 
 import (
 	"net/http"
+	"encoding/json"
 )
+
+type TrainingUnit struct {
+	Series     string
+	Techniques []Technique
+	Methods    []Method
+	Exercises  []Exercise
+}
+
+type Technique struct {
+	Kind        string
+	Name        string
+	Description string
+}
+
+type Method struct {
+	Kind        string
+	Name        string
+	Description string
+	Covers      []Technique
+}
+
+type Exercise struct {
+	Kind        string
+	Name        string
+	Description string
+}
 
 type FavIconService struct {
 }
@@ -20,9 +47,51 @@ func (s MainService) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	rw.Write([]byte("<link rel=\"shortcut icon\" href=\"http://localhost:8888/favicon.ico\" type=\"image/x-icon\">"))
 }
 
+type TrainingUnitService struct {
+}
+
+func (s TrainingUnitService) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Add("Content-Type", "application/json")
+
+	pakSao := Technique{
+		Kind:        "counter",
+		Name:        "pak sao",
+		Description: "Means slapping hand\nCounter a jab by slapping the elbow of the opponent into his body, destroying his structure\nAt the same time perform a jab",
+	}
+
+	trainingUnit := TrainingUnit{
+		Series: "JKD I",
+		Techniques: []Technique{
+			pakSao,
+		},
+		Methods: []Method{
+			{
+				Kind:        "counter",
+				Name:        "Pak Sao drill",
+				Description: "",
+				Covers:      []Technique{pakSao},
+			},
+		},
+		Exercises: []Exercise{
+			{
+				Kind:        "Sparring",
+				Name:        "Lead hand sparring",
+				Description: "Sparing with lead hand punches only",
+			},
+		},
+	}
+
+	jsonString, err := json.Marshal(trainingUnit)
+	if err != nil {
+		panic(err)
+	}
+
+	rw.Write(jsonString)
+}
 
 func main() {
 	http.Handle("/favicon.ico", FavIconService{})
 	http.Handle("/index.html", MainService{})
+	http.Handle("/training-unit", TrainingUnitService{})
 	http.ListenAndServe(":80", nil)
 }
