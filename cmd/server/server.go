aef988a6ab49fae84068b1a772ccfae901ed0f45
Daniel Schulz <daniel.schulz@ocono.de>
2018-02-09 23:58:48 +0100

    Create training units in series directory

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	repository := martialarts.FileTrainingUnitRepository{}
+	repository := martialarts.FileTrainingUnitRepository{DataPath: "/go/src/github.com/DanShu93/martialarts-tracker/data/trainingunits"}
 
 	http.Handle("/training-unit", martialarts.TrainingUnitService{Repository: repository})
 	http.ListenAndServe(":80", nil)
--- repository.go
+++ repository.go
@@ -3,6 +3,8 @@ package martialarts
 import (
 	"encoding/json"
 	"io/ioutil"
+	"fmt"
+	"os"
 )
 
 type TrainingUnitRepository interface {
@@ -10,14 +12,44 @@ type TrainingUnitRepository interface {
 }
 
 type FileTrainingUnitRepository struct {
+	DataPath string
 }
 
 func (s FileTrainingUnitRepository) Save(trainingUnit TrainingUnit) error {
+	series := trainingUnit.Series
 	jsonString, err := json.Marshal(trainingUnit)
 	if err != nil {
 		return err
 	}
 
-	return ioutil.WriteFile("/go/src/github.com/DanShu93/martialarts-tracker/data/training1.json", jsonString, 0644)
+	seriesDirectoryName := s.DataPath + "/" + series + "/"
 
+	if _, err := os.Stat(seriesDirectoryName); os.IsNotExist(err) {
+		err = os.Mkdir(seriesDirectoryName, 0744)
+		if err != nil {
+			return err
+		}
+	}
+
+	index, err := getCurrentLessonIndex(seriesDirectoryName)
+	if err != nil {
+		return err
+	}
+
+	filename := index + ".json"
+	filePath := seriesDirectoryName + filename
+
+	fmt.Println(filePath)
+
+	return ioutil.WriteFile(filePath, jsonString, 0644)
+}
+
+// TODO implement
+func getCurrentLessonIndex(seriesDirectoryName string) (string, error) {
+	_, err := ioutil.ReadDir(seriesDirectoryName)
+	if err != nil {
+		return "", err
+	}
+
+	return "1", nil
 }
